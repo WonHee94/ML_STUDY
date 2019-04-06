fc2b137f99754e8cda62298304d2c6e2c53a9fc1
Wonhee Cho <wonhee4274@gmail.com>
2019-04-06 23:00:13 +0900

    wonheeTest commit

--- 1430_wh.py
+++ 1430_wh.py
@@ -1,6 +1,4 @@
-Python 3.6.5 (v3.6.5:f59c0932b4, Mar 28 2018, 16:07:46) [MSC v.1900 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> num = []
+num = []
 n=int(input())
 for i in range(n):
     num.append(int(input().split()))
